ceb141d9869e69f44cf4aa9a932851c4e882371e
Pranav Rajaram <prajaram@ucsd.edu>
2025-03-11 19:23:12 -0700

    small changes

--- script.js
+++ script.js
@@ -483,3 +483,19 @@ function hideTooltip() {
         .style("opacity", 0)
         .style("display", "none");
 }
+
+
+// JavaScript to handle the popup
+document.getElementById('popupButton').addEventListener('click', function() {
+    document.getElementById('popup').classList.remove('hidden');
+});
+
+document.querySelector('.close').addEventListener('click', function() {
+    document.getElementById('popup').classList.add('hidden');
+});
+
+window.addEventListener('click', function(event) {
+    if (event.target == document.getElementById('popup')) {
+        document.getElementById('popup').classList.add('hidden');
+    }
+});
